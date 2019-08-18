7a9dabf2b77cc64550f9e51b45226992284980e0
imuncle <big.uncle@foxmail.com>
2019-08-18 18:45:12 +0800

    添加模型

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # live2d
-live2d模型收集+展示
+live2d模型收集+展示，可直接用于静态网站
+
+# 展示页面
+https://imuncle.github.io/live2d
+
+# 使用说明
+* 引用必要的`js`文件：
+```html
+<script src="js/LAppDefine.js"></script>
+<script src="js/live2d.min.js"></script>
+```
+* 添加`canvas`元素：
+```html
+<canvas class="live2d" id="live2d" width="300" height="800"></canvas>
+```
+* 添加`css`样式（非必须，可自定义）：
+```html
+<style>
+    .live2d {
+        position: fixed;
+        bottom : 0;
+        left : 0;
+    }
+</style>
+```
+* 初始化live2d：
+```html
+<script>InitLive2D();</script>
+```
+
+* `LAppDefine.js`为live2d的配置文件，具体的配置信息见注释。
+
+* `model`文件夹下是live2d的模型文件，目前已收录**41**种，其中数种模型有可更换皮肤。
+模型文件夹的目录大体如下：
+```txt
++-models（模型文件夹名称）
+|__+-texture（模型材质包）
+|__|___texture_00.png
+|__|___texture_01.png
+|__|___....
+|__+-motions（模型动作组）
+|__+-expression（模型表情组）
+|__+-sounds（模型音效）
+|__model.json（模型资源分布说明文件）
+|__model.moc（模型本体）
+```
+部分模型有所不同。
+
+# 相关链接
+* Live2D官方网站：
+https://www.live2d.com/en/
+https://live2d.github.io
+* [梦象](https://mx-model.ga/) （live2d模型收集站）
+* [Live2D！把可爱的看板娘捕捉到你的博客去吧](https://haremu.com/p/205)
+* [EYHN/hexo-helper-live2d](https://github.com/EYHN/hexo-helper-live2d) （向hexo中添加live2d）
+* [网页添加 Live2D 看板娘](https://www.fghrsh.net/post/123.html)
+* [Live2dDemo](https://github.com/summerscar/live2dDemo) （live2d模型收集站）
+
+# 版权须知
+所有模型均收集自互联网，版权均归原公司/个人所有。您可将本站资源用于学习、非营利性的网站或项目，不得用于商业使用（付费分发售卖资源、二次修改用于盈利等）。
---
+++ js/LAppDefine.js
@@ -0,0 +1,122 @@
+var LAppDefine = {
+    // 调试，true时会在console里显示日志
+    DEBUG_LOG: false,
+    DEBUG_MOUSE_LOG: false,
+    // 鼠标相关日志   
+    //  全部设定    
+    //这里配置canvsa元素的id
+    CANVAS_ID: "live2d",
+    //是否开启滚轮缩放，默认true
+    IS_SCROLL_SCALE: false,
+    // 画面最大缩放级别
+    VIEW_MAX_SCALE: 2,
+    // 画面最小缩放级别
+    VIEW_MIN_SCALE: 0.8,
+
+    VIEW_LOGICAL_LEFT: -1,
+    VIEW_LOGICAL_RIGHT: 1,
+
+    VIEW_LOGICAL_MAX_LEFT: -2,
+    VIEW_LOGICAL_MAX_RIGHT: 2,
+    VIEW_LOGICAL_MAX_BOTTOM: -2,
+    VIEW_LOGICAL_MAX_TOP: 2,
+
+    // 动作优先级常量
+    //关于各个事件触发的表情可以用动作常量优先级来取代
+    PRIORITY_NONE: 0,
+    PRIORITY_IDLE: 1,
+    PRIORITY_NORMAL: 2,
+    PRIORITY_FORCE: 3,
+
+    //是否允许拖拽，默认为true
+    IS_DRAGABLE: true,
+
+    //是否绑定切换模型按钮
+    IS_BIND_BUTTON: true,
+    //绑定按钮元素id
+    BUTTON_ID: "Change",
+    //是否开启模型切换完成之前禁止按钮点击的选项，默认为true
+    IS_BAN_BUTTON: true,
+    //设置按钮禁止状态时的class，可自定义样式，前提是IS_BAN_BUTTON为true
+    BAN_BUTTON_CLASS: "inactive",
+    //设置按钮正常状态时的class
+    NORMAL_BUTTON_CLASS: "active",
+
+    //皮肤切换模式 目前只支持 sequence-顺序
+    TEXURE_CHANGE_MODE: "sequence",
+    IS_START_TEXURE_CHANGE: true,
+    TEXURE_BUTTON_ID: "texure",
+    /**
+     *  模型定义
+        自定义配置模型，同一数组内放置同个模型的不同皮肤，换肤时按照顺序依次显示
+        这里请用相对路径配置
+     */
+    MODELS: [
+        ["model/22/index.json", "model/22/index-cba-normal.json", "model/22/index-cba-super.json", "model/22/index-newyear.json", "model/22/index-school2017.json", "model/22/index-spring.json", "model/22/index-summer2017-high.json", "model/22/index-summer2017-low.json", "model/22/index-tomo-high.json", "model/22/index-tomo-low.json", "model/22/index-valley2017.json", "model/22/index-vdays.json", "model/22/index-xmas.json", "model/22/index-xmas-headwear.json"], 
+        ["model/22_high/model.json"], 
+        ["model/33/index.json", "model/33/index-cba-normal.json", "model/33/index-cba-super.json", "model/33/index-newyear.json", "model/33/index-school2017.json", "model/33/index-spring.json", "model/33/index-summer2017-high.json", "model/33/index-summer2017-low.json", "model/33/index-tomo-high.json", "model/33/index-tomo-low.json", "model/33/index-valley2017.json", "model/33/index-vdays.json", "model/33/index-xmas.json", "model/33/index-xmas-headwear.json"], 
+        ["model/33_high/model.json"], 
+        ["model/bronya/model.json"], 
+        ["model/haru/haru_01.model.json", "model/haru/haru_02.model.json"], 
+        ["model/haruto/haruto.model.json"], 
+        ["model/hibiki/hibiki.model.json"], 
+        ["model/hijiki/hijiki.model.json"], 
+        ["model/index/model.json"], 
+        ["model/izumi/izumi.model.json"], 
+        ["model/katou_01/katou_01.model.json"], 
+        ["model/liang/2.json"], 
+        ["model/live_uu/model.json"], 
+        ["model/live_uu/model_usb.json"], 
+        ["model/mei/model.json"], 
+        ["model/miku/miku.model.json"], 
+        ["model/murakumo/index.json"], 
+        ["model/xiaomai/xiaomai.model.json"], 
+        ["model/wanko/wanko.model.json"], 
+        ["model/shizuku/shizuku.model.json"], 
+        ["model/Epsilon2.1/Epsilon2.1.model.json"], 
+        ["model/Pio/model.json", "model/Pio/model1.json", "model/Pio/model2.json", "model/Pio/model3.json", "model/Pio/model4.json", "model/Pio/model5.json"], 
+        ["model/platelet/model.json"], 
+        ["model/platelet-2/model.json"], 
+        ["model/rem/model.json"], 
+        ["model/sagiri/sagiri.model.json"], 
+        ["model/snow_miku/model.json"], 
+        ["model/Terisa/model.json"], 
+        ["model/Tia/index.json", "model/Tia/index1.json", "model/Tia/index2.json", "model/Tia/index3.json", "model/Tia/index4.json", "model/Tia/index5.json"], 
+        ["model/tororo/tororo.model.json"], 
+        ["model/tsumiki/tsumiki.model.json"], 
+        ["model/unitychan/unitychan.model.json"], 
+        ["model/yuri/model.json"], 
+        ["model/z16/z16.model.json"], 
+        ["model/chitose/chitose.model.json"], 
+        ["model/HyperdimensionNeptunia/blanc_classic/index.json", "model/HyperdimensionNeptunia/blanc_normal/index.json", "model/HyperdimensionNeptunia/blanc_swimwear/index.json"], 
+        ["model/HyperdimensionNeptunia/histoire/index.json", "model/HyperdimensionNeptunia/histoirenohover/index.json"], 
+        ["model/HyperdimensionNeptunia/nepgear/index.json", "model/HyperdimensionNeptunia/nepgearswim/index.json", "model/HyperdimensionNeptunia/nepgear_extra/index.json"], 
+        ["model/HyperdimensionNeptunia/nepmaid/index.json", "model/HyperdimensionNeptunia/nepnep/index.json", "model/HyperdimensionNeptunia/nepswim/index.json", "model/HyperdimensionNeptunia/neptune_classic/index.json", "model/HyperdimensionNeptunia/neptune_santa/index.json"], ["model/HyperdimensionNeptunia/noir_classic/index.json", "model/HyperdimensionNeptunia/noir/index.json", "model/HyperdimensionNeptunia/noir_santa/index.json", "model/HyperdimensionNeptunia/noireswim/index.json"], 
+        ["model/HyperdimensionNeptunia/vert_classic/index.json", "model/HyperdimensionNeptunia/vert_normal/index.json", "model/HyperdimensionNeptunia/vert_swimwear/index.json"]
+    ],
+
+    // 与外部定义的json文件匹配
+    MOTION_GROUP_IDLE: "idle",
+    // 空闲时
+    MOTION_GROUP_TAP_BODY: "tap_body",
+    // 点击身体时
+    MOTION_GROUP_FLICK_HEAD: "flick_head",
+    // 抚摸头部
+    MOTION_GROUP_PINCH_IN: "pinch_in",
+    // 放大时
+    MOTION_GROUP_PINCH_OUT: "pinch_out",
+    // 缩小时
+    MOTION_GROUP_SHAKE: "shake",
+    // 摇晃
+    //如果有自定义的动作分组可以放在这里
+    // 与外部定义json文件相匹配
+    HIT_AREA_HEAD: "head",
+    HIT_AREA_BODY: "body",
+
+    //初始化的模型大小
+    SCALE: 1.0,
+    //新增属性，是否播放音频 默认为true
+    IS_PLAY_AUDIO: true,
+    //新增属性，audio标签id值   
+    AUDIO_ID: "my_audio"
+};
